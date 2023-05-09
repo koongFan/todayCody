d69514f1fc0d4a4cdf6cd89c0f89b48e180e15f7
khan9389 <idi0tmusic93@gmail.com>
2023-05-09 16:18:50 +0900

    test : 로그인 api 작성 -로그인 API 작성

--- todayCody/src/main/java/com/example/todayCody/login/LoginDAOImpl.java
+++ todayCody/src/main/java/com/example/todayCody/login/LoginDAOImpl.java
@@ -4,6 +4,11 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
+
 @Repository
 public class LoginDAOImpl implements LoginDAO {
 
@@ -14,5 +19,14 @@ public class LoginDAOImpl implements LoginDAO {
         String userId = this.sqlSession.selectOne("com.example.todayCody.LoginDAO.getUserId");
 
         return userId;
-    };
+    }
+
+  @Override
+  public Map<String, Object> user_login(HashMap<String, Object> jsonMap, HttpServletRequest request, HttpServletResponse response) throws Exception {
+      HashMap<String, Object> retMap = new HashMap<>();
+      retMap.put("test", "성공");
+    return retMap;
+  }
+
+  ;
 }
--- todayCody/src/main/java/com/example/todayCody/login/LoginServiceImpl.java
+++ todayCody/src/main/java/com/example/todayCody/login/LoginServiceImpl.java
@@ -1,5 +1,21 @@
 package com.example.todayCody.login;
 
-public class LoginServiceImpl {
-    
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service("LoginService")
+public class LoginServiceImpl implements LoginService{
+
+  @Autowired
+  private LoginDAO loginDAO;
+
+  @Override
+  public Map<String, Object> user_login(HashMap<String, Object> jsonMap, HttpServletRequest request, HttpServletResponse response) throws Exception {
+    return loginDAO.user_login(jsonMap,request,response);
+  }
 }
