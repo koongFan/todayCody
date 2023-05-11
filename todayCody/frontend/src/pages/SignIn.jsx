36aec96bf356f202cebb81f41f274feeab787118
ore <go1ruf2tk3@gmail.com>
2023-05-11 13:28:30 +0900

    style: 로그인창 스타일링

--- todayCody/frontend/src/pages/SignIn.jsx
+++ todayCody/frontend/src/pages/SignIn.jsx
@@ -1,46 +1,86 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 export default function Login() {
-  const [user, setUser] = useState({
+  const [inputs, setInputs] = useState({
     email: "",
     password: "",
   });
-  const handleChange = (e) => {
+
+  const [valid, setValid] = useState({
+    email: false,
+    password: false,
+  });
+
+  const [formIsValid, setFormIsValid] = useState(false);
+
+  useEffect(() => {
+    const identifier = setTimeout(() => {
+      console.log("유효성 검사중");
+      setFormIsValid(inputs.email.includes("@") && inputs.password.length >= 8);
+    }, 300);
+
+    return () => {
+      console.log("클린업");
+      clearTimeout(identifier);
+    };
+  }, [inputs.email, inputs.password]);
+
+  const changeHandler = (e) => {
     const { name, value } = e.target;
-    setUser({ ...user, [name]: value });
+    setInputs({ ...inputs, [name]: value });
   };
-  const handleSubmit = (e) => {
+
+  const validHandler = (e) => {
+    const { name } = e.target;
+    if (name === "email") {
+      setValid({ ...valid, [name]: inputs.email.includes("@") });
+    } else {
+      setValid({ ...valid, [name]: inputs.password.length >= 8 });
+    }
+  };
+
+  const submitHandler = (e) => {
     e.preventDefault();
   };
 
   return (
-    <div className="signin">
-      <div className="signin-container">
-        <h3>오늘코디</h3>
-        <form onSubmit={handleSubmit}>
+    <div className="wrapper">
+      <div className="formContainer">
+        <form onSubmit={submitHandler}>
+          <h3>오늘코디</h3>
+          <p>오늘코디 계정으로 로그인</p>
           <input
             type="email"
             name="email"
-            value={user.email}
-            required
-            placeholder="email"
-            onChange={handleChange}
+            value={inputs.email}
+            onChange={changeHandler}
+            onBlur={validHandler}
+            placeholder="이메일을 입력해주세요."
           />
+          {!valid.email && <p className="warn">이메일 형식을 맞춰주세요.</p>}
           <input
             type="password"
             name="password"
-            value={user.password}
-            required
-            placeholder="password"
-            onChange={handleChange}
+            value={inputs.password}
+            onChange={changeHandler}
+            onBlur={validHandler}
+            placeholder="비밀번호를 입력해주세요. (8자 이상)"
           />
-          <button> 로그인</button>
+          {!valid.password && <p className="warn">8자리 이상 입력해주세요.</p>}
+          <button
+            type="submit"
+            disabled={!formIsValid}
+            className={`${!formIsValid && "disabled"}`}
+          >
+            로그인
+          </button>
+          <div className="aboutSign">
+            <Link to="#">비밀번호 찾기</Link>
+            <Link to="/signup">회원가입</Link>
+          </div>
         </form>
-        <p>
-          계정이 없으신가요? <Link to="/signup">회원가입</Link>
-        </p>
       </div>
     </div>
   );
