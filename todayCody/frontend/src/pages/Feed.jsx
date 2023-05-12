66f96aa087b78ed0cb1adb83ce057ad6c5a2fca8
ore <go1ruf2tk3@gmail.com>
2023-05-12 09:05:19 +0900

    style: 인스타 피드 스타일링

--- todayCody/frontend/src/pages/Feed.jsx
+++ todayCody/frontend/src/pages/Feed.jsx
@@ -29,6 +29,19 @@ export default function Feed() {
         { userId: 3, userName: "smosco", text: "와 정말 멋진 바다다." },
       ],
     },
+    {
+      id: 3,
+      userName: "pangi",
+      profileImg:
+        "https://images.unsplash.com/photo-1552058544-f2b08422138a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8cGVyc29ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=400&q=60",
+      feedImg:
+        "https://images.unsplash.com/photo-1496302662116-35cc4f36df92?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fHBlcnNvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=400&q=60",
+      likes: 73,
+      feedText: "드디어 여름이 왔다. 멋진 노을!",
+      comments: [
+        { userId: 3, userName: "smosco", text: "와 정말 멋진 바다다." },
+      ],
+    },
   ];
   return (
     <div className="wrapper">
