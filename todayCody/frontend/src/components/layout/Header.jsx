277a6c5a0ace1f00c72f56533594250465adc582
ore <go1ruf2tk3@gmail.com>
2023-05-23 20:07:39 +0900

    style: 헤더 스타일링

--- todayCody/frontend/src/components/layout/Header.jsx
+++ todayCody/frontend/src/components/layout/Header.jsx
@@ -1,5 +1,5 @@
-import React from "react";
 import { Link } from "react-router-dom";
+import { IoIosSearch } from "react-icons/io";
 
 export default function Header() {
   const menus = [
@@ -7,28 +7,35 @@ export default function Header() {
     { id: 2, item: "룩별정보", to: "/lookinfo" },
     { id: 3, item: "게시판", to: "/board" },
     { id: 4, item: "피드", to: "/feed" },
-    { id: 5, item: "마이페이지", to: "/mypage" },
   ];
 
   return (
     <header>
-      <nav>
-        <Link to="/">
-          <h1 className="logo">오늘코디</h1>
-        </Link>
-        <form>
-          <input type="text" placeholder="오늘을 어떤룩?" />
+      <Link to="/" className="left">
+        <h1 className="logo">OH!CO</h1>
+      </Link>
+      <div className="right">
+        <nav className="top">
+          <ul className="top-left">
+            {menus.map((menu) => (
+              <Link to={menu.to}>
+                <li key={menu.id} className="menuItem">
+                  {menu.item}
+                </li>
+              </Link>
+            ))}
+          </ul>
+          <div className="top-right">
+            <div>고승신님</div>
+            <button>LOGIN</button>
+            <Link to="/mypage">MYPAGE</Link>
+          </div>
+        </nav>
+        <form className="bottom">
+          <IoIosSearch className="search-icon" />
+          <input type="text" placeholder="검색어를 입력해주세요." />
         </form>
-        <ul className="menu">
-          {menus.map((menu) => (
-            <Link to={menu.to}>
-              <li key={menu.id} className="menuItem">
-                {menu.item}
-              </li>
-            </Link>
-          ))}
-        </ul>
-      </nav>
+      </div>
     </header>
   );
 }
