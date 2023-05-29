b2436d344a869d23540be378f415dcd4600275d7
박한우 <han-u@baghan-uui-MacBookPro.local>
2023-05-29 19:39:41 +0900

    feat : 피드 글 등록 기능 완료

--- todayCody/src/main/java/com/example/todayCody/feed/FeedDAOImpl.java
+++ todayCody/src/main/java/com/example/todayCody/feed/FeedDAOImpl.java
@@ -1,8 +1,29 @@
 package com.example.todayCody.feed;
 
+import lombok.extern.log4j.Log4j2;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import java.util.HashMap;
+
+@Log4j2
 @Repository("feedDAO")
-public class FeedDAOImpl {
-    
+public class FeedDAOImpl implements FeedDAO {
+
+  @Autowired
+  SqlSessionTemplate sqlSession;
+
+  @Override
+  public int insertFeedInfo(HashMap<String, Object> jsonMap) {
+    sqlSession.insert("com.example.todayCody.feed.FeedDAO.insertFeedInfo", jsonMap);
+    int newFeedSeq = sqlSession.selectOne("com.example.todayCody.feed.FeedDAO.getMaxFeedSeq");
+
+    return newFeedSeq;
+  }
+
+  @Override
+  public int insertFileInfo(HashMap<String, Object> jsonMap) {
+    return sqlSession.insert("com.example.todayCody.feed.FeedDAO.insertFileInfo", jsonMap);
+  }
 }
