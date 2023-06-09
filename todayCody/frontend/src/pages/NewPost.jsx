30e3c2e37931d8815af17355926b19d71e7bba0b
raagdol <ju8559@naver.com>
2023-06-09 22:06:32 +0900

    fix: 1개의 input태그 사용

--- todayCody/frontend/src/pages/NewPost.jsx
+++ todayCody/frontend/src/pages/NewPost.jsx
@@ -10,14 +10,18 @@ export default function NewPost() {
 
   const handleFileSelect = (event) => {
     const newFiles = event.target.files; // 선택한 파일들
-
-    // 선택한 파일들을 배열에 추가
-    const updatedFiles = [...selectedFiles];
-    for (let i = 0; i < newFiles.length; i++) {
-      updatedFiles.push(newFiles[i]);
+  
+    if (newFiles.length + selectedFiles.length > 5) {
+      // 선택한 파일 개수가 5를 초과하는 경우
+      alert("최대 5개의 파일까지 선택할 수 있습니다.");
+      event.target.value = null; // 파일 선택 초기화
+      return;
     }
-    setSelectedFiles(updatedFiles); // 선택한 파일들을 상태 변수에 저장
-
+  
+    // 선택한 파일들을 배열에 추가
+    const updatedFiles = [...selectedFiles, ...newFiles];
+    setSelectedFiles(updatedFiles);
+  
     // 파일 미리보기 이미지 URL 생성
     const previews = [];
     for (let i = 0; i < updatedFiles.length; i++) {
@@ -32,6 +36,8 @@ export default function NewPost() {
       reader.readAsDataURL(file);
     }
   };
+  
+  
 
   const handleContentChange = (event) => {
     setFeedContent(event.target.value); // 피드 내용 상태 업데이트
@@ -40,25 +46,28 @@ export default function NewPost() {
   const handleUpload = async (e) => {
     if (selectedFiles.length > 0) {
       const formData = new FormData();
-
+  
       selectedFiles.forEach((fileData, index) => {
         formData.append('file[]', fileData, fileData.name); // 파일 추가
         formData.append('order_num', index + 1); // 파일 순서 추가
       });
-
+  
       const feedData = {
         user_seq: 1, // 사용자 고유 번호 (하드 코딩 예시)
         content: feedContent // 피드 내용
       };
       formData.append('jsonData', JSON.stringify(feedData));
-
+  
       try {
         setUploading(true); // 파일 업로드 시작 시 상태 업데이트
-
+  
         const response = await axios.post('/feed/write.do', formData);
         console.log(response.data);
         // 요청 성공 시 동작
-
+  
+        setSelectedFiles([]); // 선택한 파일 초기화
+        setFilePreviews([]); // 미리보기 초기화
+        setFeedContent(""); // 피드 내용 초기화
         setUploading(false); // 파일 업로드 완료 시 상태 업데이트
       } catch (error) {
         console.log(error);
@@ -67,6 +76,8 @@ export default function NewPost() {
       }
     }
   };
+  
+  
 
   const handlePreviewRemove = (index) => {
     const updatedFiles = [...selectedFiles];
@@ -82,14 +93,13 @@ export default function NewPost() {
       <div className="wrapper">
         <h1>New Post</h1>
         <div className="file-upload">
-          {[...Array(5)].map((_, index) => (
-            <input
-              key={index}
-              type="file"
-              onChange={handleFileSelect}
-              accept="image/*"
-            />
-          ))}
+          <input
+            type="file"
+            onChange={handleFileSelect}
+            accept="image/*"
+            multiple  // 다중 파일 선택 가능하도록 설정
+            maxLength={5} // 최대 5개 파일까지 선택 가능
+          />
           {filePreviews.length > 0 && (
             <div className="image-preview">
               {filePreviews.map((preview, index) => (
