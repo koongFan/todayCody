e8a840e82347288468a451284c943bb4a655210e
ore <go1ruf2tk3@gmail.com>
2023-06-17 15:37:27 +0900

    refactor: 회원가입 리팩토링

--- todayCody/frontend/src/api/auth.js
+++ todayCody/frontend/src/api/auth.js
@@ -4,22 +4,36 @@ import { useState, useEffect } from "react";
 const baseUrl = "http://52.78.103.73:8081";
 
 //회원가입
-export const signup = async (userData) => {
+export const signup = async (user) => {
   try {
-    const res = await axios({
-      url: `${baseUrl}/member/join`,
-      method: "post",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      data: userData,
-    });
-    if (res.status === 200) {
-      alert("회원가입 성공하셨습니다!");
+    const res = await axios.post(`${baseUrl}/member/signUp.do`, user);
+    console.log(res.data);
+
+    if (res.data.failOrSucc) {
+      alert("회원가입이 완료되었습니다.");
       window.location.replace("/signin");
+    } else {
+      alert("회원가입에 실패했습니다. 다시 시도해주세요." + res.data.msg);
     }
-  } catch (error) {
-    alert("회원가입에 실패했습니다");
+  } catch (err) {
+    console.log(err);
+    alert("회원가입에 실패했습니다. 다시 시도해주세요.");
+  }
+};
+
+//이메일 인증
+export const authEamil = async (email) => {
+  try {
+    const res = await axios.post(`${baseUrl}/signup/mailConfirm.do`, { email });
+    if (res.data.success) {
+      console.log("전송 성공");
+      alert("이메일로 인증코드가 전송되었습니다.");
+    } else {
+      alert("인증 코드 전송에 실패했습니다.");
+    }
+  } catch (err) {
+    console.log(err);
+    alert("인증 코드 전송에 실패했습니다.");
   }
 };
 
@@ -37,8 +51,8 @@ export const signin = async (userData) => {
       const token = res.data.token;
 
       localStorage.setItem("token", token);
-      const expiration = new Date();
-      expiration.setHours(expiration.getHours() + 0.5); //만료시간 30분
+      let expiration = new Date();
+      expiration.setHours(expiration.getHours() + 1); //만료시간 30분
       localStorage.setItem("expiration", expiration.toISOString());
 
       // API 요청하는 콜마다 헤더에 accessToken 담아 보내도록 설정
