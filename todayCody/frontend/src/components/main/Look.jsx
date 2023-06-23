e9afec9914088e965864a8c22bc433f2bb775aa8
ore <go1ruf2tk3@gmail.com>
2023-06-23 23:53:55 +0900

    style: 메인 스타일링

--- todayCody/frontend/src/components/main/Look.jsx
+++ todayCody/frontend/src/components/main/Look.jsx
@@ -1,15 +1,22 @@
-export default function Look({ look }) {
+export default function Look({ look, category }) {
   const { img, tag } = look;
   return (
     <div className="look-content">
       <div className="img-container">
         <img src={img} alt="look-img" />
       </div>
+      <div className="likes">
+        <img src="/icons/heart.svg" className="haert" alt="heart-icon" />
+
+        <p>23</p>
+      </div>
+      {category === "coder" && <img src="/icons/star.png" alt="star-img" />}
       <ul className="tags">
         {tag.map((item) => (
           <li>#{item}</li>
         ))}
       </ul>
+      <div className="user">김보로꼬리(30대)</div>
     </div>
   );
 }
--- todayCody/frontend/src/components/main/Trend.jsx
+++ todayCody/frontend/src/components/main/Trend.jsx
@@ -1,12 +1,19 @@
 import Look from "./Look";
 
-export default function Trend({ trend, title }) {
+export default function Trend({ trend, title, category }) {
   return (
     <div className="trend-container">
-      <h3 className="trend-title">{title}</h3>
+      {category === "trend" ? (
+        <h3 className="trend-title">{title}</h3>
+      ) : (
+        <div className="title">
+          <h2>이번주 오늘코더</h2>
+          <p>(23.05.07~23.05.13)</p>
+        </div>
+      )}
       <div className="trend-look">
         {trend.map((look) => (
-          <Look look={look} />
+          <Look look={look} category={category} />
         ))}
       </div>
     </div>
