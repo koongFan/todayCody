b8eb006089dafeb13190b95c1b1f90000a476f76
ore <go1ruf2tk3@gmail.com>
2023-06-23 13:22:07 +0900

    style: 헤더 스타일링

--- todayCody/frontend/src/components/layout/Header.jsx
+++ todayCody/frontend/src/components/layout/Header.jsx
@@ -11,41 +11,34 @@ export default function Header() {
     { id: 2, name: "코디랭킹", value: "rank", to: "/ranking" },
     { id: 3, name: "룩별정보", value: "info", to: "/lookInfo" },
     { id: 4, name: "게시판", value: "board", to: "/board" },
-    { id: 5, name: "피드", value: "feed", to: "feed" },
+    { id: 5, name: "피드", value: "feed", to: "/feed" },
+    { id: 5, name: "마이페이지", value: "user", to: "/mypage" },
   ];
   return (
     <>
       <header>
-        <button
-          onClick={() => {
-            setOpen((open) => !open);
-          }}
-          className="menu-icon-btn"
-          data-menu-icon-btn
-        >
-          <img src="/icons/menu.svg" className="menu-icon" alt="menu-icon" />
-        </button>
-        {/* <Link to="/">
-    <h1 className="logo">OH!CO</h1>
-  </Link>
-  <div className="right">
-    <nav className="top">
-      <ul className="top-left">
-        {menus.map((menu) => (
-          <li key={menu.id} className="menuItem">
-            <Link to={menu.to}>{menu.item}</Link>
-          </li>
-        ))}
-      </ul>
-      <div className="top-right">
-        <div>고승신님</div>
-        {!token && (
-          <li>
-            <Link to="/signin">LOGIN</Link>
-          </li>
-        )}
-        {token && (
-          <li>
+        <div className="head-left">
+          <button
+            onClick={() => {
+              setOpen((open) => !open);
+            }}
+            className="menu-icon-btn"
+            data-menu-icon-btn
+          >
+            <img src="/icons/menu.svg" className="menu-icon" alt="menu-icon" />
+          </button>
+          <Link to="/" className="logo">
+            <img src="/icons/logo.png" alt="logo" />
+          </Link>
+        </div>
+        <div className="head-right">
+          <form className="search">
+            <input type="text" />
+            <img src="/icons/search.svg" alt="search-icon" />
+          </form>
+          <Link to="/mypage">마이페이지</Link>
+          <div>고승신님</div>
+          {token ? (
             <button
               onClick={() => {
                 localStorage.removeItem("token");
@@ -53,18 +46,12 @@ export default function Header() {
                 window.location.href = "/"; //redirect, navigate으로 하면 새로고침이 안됨
               }}
             >
-              LOGOUT
+              Logout
             </button>
-          </li>
-        )}
-        <Link to="/mypage">MYPAGE</Link>
-      </div>
-    </nav>
-    <form className="bottom">
-      <IoIosSearch className="search-icon" />
-      <input type="text" placeholder="검색어를 입력해주세요." />
-    </form>
-  </div> */}
+          ) : (
+            <Link to="/signin">Login</Link>
+          )}
+        </div>
       </header>
       <aside className={`${open ? "sidebar open" : "sidebar"}`}>
         <div className="sidebar-container">
