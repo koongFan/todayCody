a01f7e17e29ad5ee6501ac5a35d9682b8f13c78b
ore <go1ruf2tk3@gmail.com>
2023-06-27 15:42:23 +0900

    feat: 피드 리스트 api 요청

--- todayCody/frontend/src/pages/Feed.jsx
+++ todayCody/frontend/src/pages/Feed.jsx
@@ -5,35 +5,13 @@ export default function Feed() {
   const feeds = useGetFeeds();
   const feedDatas = [
     {
-      id: 1,
-      nickname: "꿀맛나는샐러리",
+      feed_seq: 1,
+      u_nickname: "꿀맛나는샐러리",
       profileImg: "/img/p1.png",
-      feedImgs: ["/img/p2.png", "/img/p3.png"],
+      image_path: ["/img/p2.png", "/img/p3.png"],
       likes: 1003,
-      feedText:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt quos cum sint assumenda vero voluptatem quia ipsam eum. ",
-      tags: ["스포티", "미니멀", "클래식"],
-      comments: [
-        {
-          userId: 1,
-          nickname: "nickname",
-          text: "Lorem ipsum dolor sit amet",
-        },
-        {
-          userId: 2,
-          nickname: "nickname",
-          text: "Lorem ipsum dolor sit amet",
-        },
-      ],
-    },
-    {
-      id: 2,
-      nickname: "꿀맛나는샐러리",
-      profileImg: "/img/p1.png",
-      feedImgs: ["/img/p1.png", "/img/p2.png"],
-      likes: 1003,
-      feedText:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt quos cum sint assumenda vero voluptatem quia ipsam eum. ",
+      content:
+        "<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt quos cum sint assumenda vero voluptatem quia ipsam eum.</p>",
       tags: ["스포티", "미니멀", "클래식"],
       comments: [
         {
@@ -49,11 +27,37 @@ export default function Feed() {
       ],
     },
   ];
+  const feedLists = [
+    {
+      feed_seq: 1,
+      u_nickname: "꿀맛나는샐러리",
+      image_path: ["/img/p2.png", "/img/p3.png"],
+      likes: 1003,
+      content:
+        "<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt quos cum sint assumenda vero voluptatem quia ipsam eum.</p>",
+    },
+    {
+      feed_seq: 1,
+      u_nickname: "꿀맛나는샐러리",
+      image_path: ["/img/p2.png", "/img/p3.png"],
+      likes: 1003,
+      content:
+        "<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt quos cum sint assumenda vero voluptatem quia ipsam eum.</p>",
+    },
+    {
+      feed_seq: 1,
+      u_nickname: "꿀맛나는샐러리",
+      image_path: ["/img/p2.png", "/img/p3.png"],
+      likes: 1003,
+      content:
+        "<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt quos cum sint assumenda vero voluptatem quia ipsam eum.</p>",
+    },
+  ];
   return (
     <div className="wrapper">
       <ul className="feedList">
-        {feedDatas.map((feed) => (
-          <FeedList key={feed.id} data={feed} />
+        {feeds.map((feed) => (
+          <FeedList key={feed.feed_seq} data={feed} />
         ))}
       </ul>
     </div>
