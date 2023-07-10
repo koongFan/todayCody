d459c39b1da9d03f3124b6943c993f7b56e09135
ore <go1ruf2tk3@gmail.com>
2023-07-10 21:58:12 +0900

    style: wrapper min-height 수정

--- todayCody/frontend/src/pages/NewPost.jsx
+++ todayCody/frontend/src/pages/NewPost.jsx
@@ -137,7 +137,10 @@ export default function NewPost() {
                 />
               ) : (
                 <label htmlFor="img-input" className="img-input">
-                  <img src="/icons/photo.png" alt="" className="addPhoto" />
+                  <div className="imgContainer">
+                    <img src="/icons/photo.png" alt="" className="camera" />
+                    <img src="/icons/add.png" alt="" className="plus" />
+                  </div>
                   <p>아이콘을 클릭해서 사진을 추가해주세요!</p>
                 </label>
               )}
