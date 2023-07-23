4021a2ab56185f171a9c995ee9a666b02b7f202c
koongFan <dogehj191223@gmail.com>
2023-07-23 15:19:53 +0900

    feat: feedList 불러오기 & mypage & 회원가입 스웨거 적용 및 mypage 공통 JSON 리턴 작업

--- todayCody/src/main/java/com/example/todayCody/mypage/MypageController.java
+++ todayCody/src/main/java/com/example/todayCody/mypage/MypageController.java
@@ -3,35 +3,64 @@ package com.example.todayCody.mypage;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.todayCody.common.config.TodayCodyConstUrl;
-import com.example.todayCody.login.dto.SignRequest;
+import com.example.todayCody.common.util.ResultInfo;
+import com.example.todayCody.common.util.ReturnJsonUtil;
 
-import java.util.HashMap;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+
 import java.util.List;
-import java.util.Map;
-import lombok.extern.log4j.Log4j2;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
 
-@Log4j2
 @RestController
 @CrossOrigin
 public class MypageController {
+
+	Logger logger = LoggerFactory.getLogger(this.getClass());
     
     @Autowired
     MypageService mypageService;
 
     // 마이페이지 불러오기
+    @ApiOperation(value="마이페이지 불러오기")
+    @ApiImplicitParam(
+        name="user_seq"
+        ,value="사용자고유번호"
+      )
     @GetMapping(TodayCodyConstUrl.myPage)
-    public Object doSelectMypageList(HttpServletRequest request, HttpServletResponse response, MypageDTO info) throws Exception {
+    public JSONObject doSelectMypageList(HttpServletRequest request, HttpServletResponse response, MypageDTO info) throws Exception {
+        JSONObject jsonObject = new JSONObject();
 
-        List<MypageDTO> myPageList = mypageService.doSelectMypageList(info);
+        if("".equals(info.getUser_seq())){
+			jsonObject.put("result", JSONObject.fromObject(new ResultInfo("1002")));
+			return jsonObject;
+        }
 
-        return myPageList;
+        try{
+
+            List<MypageDTO> myPageList = mypageService.doSelectMypageList(info);
+			
+			String[] filterList = new String[] {
+					"feed_seq", "user_seq", "content", "likes", "comment", "image_path", "u_nickname", "postCnt"
+			};
+
+            jsonObject = ReturnJsonUtil.getJson("0", myPageList.size(), JSONArray.fromObject(myPageList), filterList);
+
+        } catch(Exception ex){
+			logger.error(ex.getMessage());
+			jsonObject.put("result", JSONObject.fromObject(new ResultInfo("1500")));
+        }
+
+        return jsonObject;
     }
 
 }
